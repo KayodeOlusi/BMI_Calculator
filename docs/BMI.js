6b99f8d38987da4aec097097848cfa56ed0bb3b3
KayodeOlusi <kayodeolusi@gmail.com>
2021-02-09 20:09:39 +0100

    Updated the other pages

---
+++ docs/BMI.js
@@ -0,0 +1,19 @@
+var newPromt = prompt('Enter your name')
+document.getElementById('submit').addEventListener('click',bmiCalculator);
+
+//the bmiCalculator Funcion
+
+function bmiCalculator(){
+    var cm = parseInt(document.getElementById('cm').value);//converts the inputed value to a number
+    var kg = parseFloat(document.getElementById('kg').value); // converts the inputed value to a floated number 
+
+    var bmi;
+    var newCm = parseFloat(cm/100);
+    bmi = kg / (newCm * newCm);
+ 	bmi = bmi.toFixed(1);//converts every exponential or complex form of numbers to digits
+    console.log(bmi);
+    
+    document.getElementById('result').innerHTML = newPromt +" "+ "your BMI result is" +" "+ bmi;
+
+
+}
